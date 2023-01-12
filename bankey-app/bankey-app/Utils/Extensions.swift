9d6b2e2d21739ea9e955b795f2017ab529767a9b
Dinara Zimanova <madendinara@gmail.com>
2023-01-12 02:08:52 -0500

    Refactoring tab bar

--- bankey-app/bankey-app/Utils/Extensions.swift
+++ bankey-app/bankey-app/Utils/Extensions.swift
@@ -9,14 +9,12 @@ import UIKit
 
 extension UIViewController {
     func setStatusBar() {
-        let statusBarSize = UIApplication.shared.statusBarFrame.size
-        let frame = CGRect(origin: .zero, size: statusBarSize)
-        let statusView = UIView(frame: frame)
-        
-        statusView.backgroundColor = .systemTeal
-        view.addSubview(statusView)
-        
-    }
+            let navBarAppearance = UINavigationBarAppearance()
+            navBarAppearance.configureWithTransparentBackground() // to hide Navigation Bar Line also
+        navBarAppearance.backgroundColor = .systemTeal
+            UINavigationBar.appearance().standardAppearance = navBarAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+        }
     
     func setTabBarImage(imageName: String, title: String) {
         let configuration = UIImage.SymbolConfiguration(scale: .large)
